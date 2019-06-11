de841f90c2f4820c476b352dd21d89d08d089018
Jay Matta <C-Jay.Matta@charter.com>
2019-06-11 09:24:28 -0500

    Testing Jenkins pipleine

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,3 +1,22 @@
-@Library('CICD-SharedLib') _
-
-test()
+pipeline {
+    agent any
+    stages {
+        stage('Test Variables') {
+          steps {
+            sh 'echo ${GIT_BRANCH}'
+            sh 'echo ${BRANCH_NAME}'
+            sh 'echo ${JOB_BASE_NAME}'
+            sh 'echo ${COMPRESSEDBUILD}'
+          	}
+        }
+        stage('Deploy') {
+            steps {
+                script {
+                    sh 'echo ---- Attempting to deploy application to ${GIT_BRANCH} environment servers. ----'
+                    sh 'echo ---- Current branch being deployed: ${GIT_BRANCH} ----'
+                    sh './deploy.sh'
+                }
+            }
+        }
+    }
+}
---
+++ deploy.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -x
+
+if [ $1 == 'development' ] 
+then
+        URLS=("35.231.92.12" "34.73.24.245")
+elif [[ $1 == *release* ]]
+then
+        URLS=("34.73.24.245")    
+else 
+   exit 1
+fi
+
+#URLS=("pub1-dev1.ncw.webapps.rr.com")
+SALESCHANNELS=("DS")
+
+ARTIFACT_NAME="apache-tomcat-8.5.9.tar.gz"
+ARTIFACT_PATH="dist/tomcat/tomcat-8/v8.5.9/bin/"
+SERVERS=( "${URLS[@]}" )
+
+for SALESCHANNEL in "${SALESCHANNELS[@]}"
+do
+if [ $SALESCHANNEL == "DS" ]
+    then
+        APP_HOME="/opt/apps/www/mobile-token-generator-https"
+        CONFIG_NAME="apache-tomcat-8.5.9.tar.gz"
+      else 
+                exit 1
+    # else
+    #     APP_HOME="/data/apps/www/retail-mobile-token-generator-https"
+    #     CONFIG_NAME="config.development.retail.js"
+fi
+done
+UI__HOME_TEMP="${APP_HOME}/tmp"
+
+for SERVER in "${SERVERS[@]}"
+do
+echo "Deploying to $SERVER"
+#ssh -o StrictHostKeyChecking=no jayasimhaiit@${SERVER} << END_CONNECTION
+ssh jayasimhaiit@${SERVER} << END_CONNECTION
+    # Clear the old version
+    rm -rf ${APP_HOME}/*
+
+    # Build an expansion directory
+    mkdir -p ${UI__HOME_TEMP}
+
+    # Get and expand the new version
+    wget --no-check-certificate --no-verbose -P ${UI__HOME_TEMP} https://archive.apache.org/${ARTIFACT_PATH}/${ARTIFACT_NAME}
+    tar -xzf ${UI__HOME_TEMP}/${ARTIFACT_NAME} -C ${UI__HOME_TEMP}
+
+    # Move the needed files from the temp folder to the root
+    mv ${UI__HOME_TEMP}/* ${APP_HOME}
+    rm -rf ${UI__HOME_TEMP}
+
+    # Use correct config version
+    mv ${APP_HOME}/${CONFIG_NAME} ${APP_HOME}/config.tar.gz
+
+    # Make sure owner is correct
+    chown -R jayasimhaiit:jayasimhaiit ${APP_HOME}/
+
+END_CONNECTION
+done
